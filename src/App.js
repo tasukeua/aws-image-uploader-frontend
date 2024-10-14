b2faa840254a75983bef7ad3e4b0c242a32ccdbb
Oleksii Rozumenko <oleksii.rozumenko@gmail.com>
2024-10-14 19:49:14 +0300

    Get users from back

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ import './App.css';
 import axios from "axios";
 
 const User = () => {
+    const [users, setUsers] = useState([])
     const fetchUser = () => {
         axios.get("http://localhost:8080/api/v1/user/all").then(res => {
             console.log(res)
+            setUsers(res.data)
         })
     }
     useEffect(() => {
         fetchUser()
     }, [])
 
-    return <h1>Hello</h1>
+    return users.map((user, index) => {
+            return (
+                <div key={index}>
+                    <h1>{user.username}</h1>
+                    <p>{user.id}</p>
+                </div>
+            )
+        }
+    )
 }
 
 function App() {
