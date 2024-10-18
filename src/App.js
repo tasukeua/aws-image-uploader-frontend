4d5923dfdb00234f4152c5f7385569d1c09dfafa
Oleksii Rozumenko <oleksii.rozumenko@gmail.com>
2024-10-18 14:27:04 +0300

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useCallback} from "react";
 import './App.css';
 import axios from "axios";
+import {useDropzone} from 'react-dropzone'
 
 const User = () => {
     const [users, setUsers] = useState([])
@@ -17,11 +18,53 @@ const User = () => {
     return users.map((user, index) => {
             return (
                 <div key={index}>
+                    {user.id ? (
+                        <img
+                            src={`http://localhost:8080/api/v1/user/${user.id}/image/download`}
+                            alt={'User'}/>
+                    ) : null}
+                    <br/>
+                    <br/>
                     <h1>{user.username}</h1>
                     <p>{user.id}</p>
+                    <Dropzone userId={user.id}/>
+                    <br/>
                 </div>
             )
         }
+    )
+}
+
+function Dropzone({userId}) {
+    const onDrop = useCallback(acceptedFiles => {
+        const file = acceptedFiles[0]
+        console.log(file)
+        const formData = new FormData()
+        formData.append('file', file)
+        axios.post(`http://localhost:8080/api/v1/user/${userId}/image/upload`,
+            formData,
+            {
+                headers: {
+                    "Content-Type": "multipart/form-data"
+                }
+            }
+        ).then(() => {
+            console.log('File uploaded successfully')
+        }).catch(err => {
+            console.log('File was not uploaded, error: ' + err)
+        })
+    }, [userId])
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+    return (
+        <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            {
+                isDragActive ?
+                    <p>Drop the image here ...</p> :
+                    <p>Drag 'n' drop user image, or click to select image</p>
+            }
+        </div>
     )
 }
 
